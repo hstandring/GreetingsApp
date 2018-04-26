722bd54c58631a682a4c66fd0261dddb29c661a6
Heather <hstandring88@gmail.com>
2018-04-25 23:19:31 -0400

    Enable refreshing

--- src/main/java/org/galatea/samples/controller/GreetingsController.java
+++ src/main/java/org/galatea/samples/controller/GreetingsController.java
@@ -1,6 +1,7 @@
 package org.galatea.samples.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 public class GreetingsController {
 
